f75b4f4c0c68e4b460bae6c2d34b397d380fe761
Burfi <burfi30@gmx.de>
2025-05-09 16:28:49 +0200

    Implemented output recursively

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 class Main {
+
+    public static List<Input> allElements = List.of(
+            new Input(7, 5, "nested22"),
+            new Input(6, 5, "nested21"),
+            new Input(5, 1, "subfolder2"),
+            new Input(1, null, "root"),
+            new Input(2, 1, "subfolder1"),
+            new Input(3, 2, "nested11"),
+            new Input(4, 2, "nested12")
+    );
+
     static class Input {
         public Integer id;
         public Integer parent;
@@ -28,15 +39,21 @@ class Main {
              6 nested21
              7 nested22
         */
+        var first = allElements.stream().filter(input -> input.parent == null).findFirst();
+        if (first.isPresent()) {
+            outputRecursively(first.get(), 0);
+        } else {
+            System.out.println("Not a tree;");
+        }
+    }
 
-        var input = new ArrayList<Input>(Arrays.asList(
-                new Input(7, 5, "nested22"),
-                new Input(6, 5, "nested21"),
-                new Input(5, 1, "subfolder2"),
-                new Input(1, null, "root"),
-                new Input(2, 1, "subfolder1"),
-                new Input(3, 2, "nested11"),
-                new Input(4, 2, "nested12")
-        ));
+
+    public static void outputRecursively(Input currentLevel, int indent) {
+        System.out.println(" ".repeat(indent) + currentLevel.id + " " + currentLevel.name);
+        allElements.stream()
+                .filter(input -> currentLevel.id.equals(input.parent))
+                .sorted(Comparator.comparing(o -> o.name))
+                .forEach(l -> outputRecursively(l, indent + 2));
+
     }
 }
